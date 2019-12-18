6ab10a0c937e996c417f8d1dc683b0250c8557ad
Peter Cheng <petercheng00@gmail.com>
2019-12-17 23:49:39 -0800

    days 14 and 15

--- day14/day14.py
+++ day14/day14.py
@@ -1,4 +1,71 @@
-# here is the plan
-# for each element, we have a recipe that can produce that element
-# the goal is to build up, for each element, the minimum fuel cost to build that element
-# for each element, we will know minimum fuel cost to build that element
+import math
+from collections import defaultdict
+# elements are keys, values are (<num output produced>, [(input element, amount), (input element, amount), ...]
+recipes = {}
+with open('input') as f:
+    recipes_str = f.readlines()
+    for recipe in recipes_str:
+        recipe = recipe.replace(',', '')
+        recipe = recipe.split()
+        output = recipe[-1]
+        output_amount = int(recipe[-2])
+        inputs = []
+        for i in range(0, len(recipe)-3, 2):
+            inputs.append((recipe[i+1], int(recipe[i])))
+        recipes[output] = (output_amount, inputs)
+
+def getOreCostsForFuel(fuel_amount):
+    ore_costs = 0
+    required = {'FUEL': fuel_amount}
+    surplus = defaultdict(int)
+
+    while len(required) > 0:
+        required_element, required_amount = required.popitem()
+        if required_element == 'ORE':
+            ore_costs += required_amount
+            continue
+        output_amount, inputs = recipes[required_element]
+        num_copies = math.ceil(required_amount / output_amount)
+        for input_element, input_amount in inputs:
+            input_amount *= num_copies
+            if surplus[input_element] <= input_amount:
+                # use up all the surplus
+                input_amount -= surplus[input_element]
+                surplus[input_element] = 0
+            else:
+                # surplus can satisfy this input requirement
+                surplus[input_element] -= input_amount
+                continue
+            if input_element in required:
+                required[input_element] += input_amount
+            else:
+                required[input_element] = input_amount
+
+        # we might have some extra output
+        surplus[required_element] += (output_amount * num_copies - required_amount)
+    return ore_costs
+
+print(f'{getOreCostsForFuel(1)} ORE => 1 FUEL')
+
+
+# we want to plug in different input numbers until the output number equals 1,000,000,000,000
+# first find an upper limit
+target = 1000000000000
+lower_bound = 1
+upper_bound = 1
+while getOreCostsForFuel(upper_bound) < target:
+    upper_bound *= 10
+
+while upper_bound > lower_bound+1:
+    middle = (lower_bound + upper_bound) // 2
+    middle_cost = getOreCostsForFuel(middle)
+    if middle_cost == target:
+        print(f'{middle_cost} ORE => {target} FUEL')
+        break
+    elif middle_cost <= target:
+        lower_bound = middle
+    else:
+        upper_bound = middle
+
+print(f'{getOreCostsForFuel(lower_bound)} ORE => {lower_bound} FUEL')
+print(f'{getOreCostsForFuel(upper_bound)} ORE => {upper_bound} FUEL')
---
+++ day15/day15.py
@@ -0,0 +1,173 @@
+from collections import defaultdict
+import itertools
+with open('input') as f:
+    ints = f.readline().split(',')
+
+# remove the newline
+ints[-1] = ints[-1].strip()
+ints = [int(i) for i in ints]
+
+class Computer:
+    def __init__(self, program, inputs):
+        self.program = program.copy()
+        self.inputs = inputs.copy()
+        self.halted = False
+        self.error = False
+        self.position = 0
+        self.relative_base = 0
+        self.outputs = []
+        self.extra_memory = {}
+
+    def read(self, addr):
+        if addr < len(self.program):
+            return self.program[addr]
+        if addr in self.extra_memory:
+            return self.extra_memory[addr]
+        return 0
+
+    def write(self, addr, value):
+        if addr < len(self.program):
+            self.program[addr] = value
+        else:
+            self.extra_memory[addr] = value
+
+    def getSrcParam(self, position, param_modes, param_index):
+        param_mode = param_modes[param_index] if param_index < len(param_modes) else 0
+        param_val = self.read(position)
+        if param_mode == 0:
+            return self.read(param_val)
+        elif param_mode == 1:
+            return param_val
+        elif param_mode == 2:
+            return self.read(self.relative_base + param_val)
+        else:
+            print(f'bad param mode {param_mode}')
+
+    def getDstParam(self, position, param_modes, param_index):
+        param_mode = param_modes[param_index] if param_index < len(param_modes) else 0
+        param_val = self.read(position)
+        if param_mode == 0:
+            return param_val
+        elif param_mode == 2:
+            return self.relative_base + param_val
+        else:
+            print(f'bad param mode {param_mode}')
+
+    # execute ends either when waiting for an input or halted
+    def execute(self):
+        while True:
+            instruction = self.program[self.position]
+            opcode = int(str(instruction)[-2:])
+            param_modes = [int(d) for d in str(instruction)[:-2]]
+            param_modes.reverse()
+            # print(f'instruction is {instruction}, opcode is {opcode}, param_modes are {param_modes}')
+            if opcode == 1 or opcode == 2: #add, mul
+                src1 = self.getSrcParam(self.position+1, param_modes, 0)
+                src2 = self.getSrcParam(self.position+2, param_modes, 1)
+                dst = self.getDstParam(self.position+3, param_modes, 2)
+                if opcode == 1:
+                    self.write(dst, src1 + src2)
+                else:
+                    self.write(dst, src1 * src2)
+                self.position += 4
+            elif opcode == 3: #input
+                if len(self.inputs) == 0:
+                    break
+                dst = self.getDstParam(self.position+1, param_modes, 0)
+                self.write(dst, self.inputs.pop(0))
+                self.position += 2
+            elif opcode == 4: #output
+                src = self.getSrcParam(self.position+1, param_modes, 0)
+                self.outputs.append(src)
+                self.position += 2
+            elif opcode == 5 or opcode == 6: #jumpiftrue, jumpiffalse
+                src1 = self.getSrcParam(self.position+1, param_modes, 0)
+                src2 = self.getSrcParam(self.position+2, param_modes, 1)
+                if opcode == 5 and src1 != 0:
+                    self.position = src2
+                elif opcode == 6 and src1 == 0:
+                    self.position = src2
+                else:
+                    self.position += 3
+            elif opcode == 7 or opcode == 8: #lt, equals
+                src1 = self.getSrcParam(self.position+1, param_modes, 0)
+                src2 = self.getSrcParam(self.position+2, param_modes, 1)
+                dst = self.getDstParam(self.position+3, param_modes, 2)
+                if opcode == 7:
+                    self.write(dst, 1 if src1 < src2 else 0)
+                elif opcode == 8:
+                    self.write(dst, 1 if src1 == src2 else 0)
+                self.position += 4
+            elif opcode == 9: # set relative base
+                src = self.getSrcParam(self.position+1, param_modes, 0)
+                self.relative_base += src
+                self.position += 2
+            elif opcode == 99:
+                # print('halt')
+                self.halted = True
+                break
+            else:
+                print('error')
+                self.error = True
+                self.halted = True
+                break
+
+
+# the plan will be to do bfs. for each valid set of inputs so far, we will try adding each of the 4 inputs to make new set of valid inputs. we will prune out any input sequences that hit a wall or do backtracking
+
+def getVisitedCoords(inputs):
+    vc = [(0,0)]
+    for i in inputs:
+        x, y = vc[-1] # y is north
+        if i == 1: # north
+            vc.append((x, y+1))
+        elif i == 2: # south
+            vc.append((x, y-1))
+        elif i == 3: # west
+            vc.append((x-1, y))
+        elif i == 4: # east
+            vc.append((x+1, y))
+    return vc
+
+def repeated_coord(vc_set, last_coord, i):
+    x, y = last_coord
+    if i == 1: # north
+        return (x, y+1) in vc_set
+    elif i == 2: # south
+        return (x, y-1) in vc_set
+    elif i == 3: # west
+        return (x-1, y) in vc_set
+    elif i == 4: # east
+        return (x+1, y) in vc_set
+
+def submitInputs(inputs):
+    c = Computer(ints, inputs)
+    c.execute()
+    return c.outputs[-1]
+
+valid_inputs=[[]]
+done = False
+while not done:
+    print(f'num valid: {len(valid_inputs)} input size: {len(valid_inputs[0])}')
+    next_valid_inputs = []
+    for valid_input in valid_inputs:
+        visited_coords = getVisitedCoords(valid_input)
+        last_coord = visited_coords[-1]
+        vc_set = set(visited_coords)
+        for direction in [1, 2, 3, 4]:
+            if repeated_coord(vc_set, last_coord, direction):
+                continue
+            new_input = valid_input + [direction]
+            result = submitInputs(new_input)
+            if result == 0:
+                continue
+            elif result == 1:
+                next_valid_inputs.append(new_input)
+            elif result == 2:
+                print(new_input)
+                print(len(new_input))
+                done = True
+                break
+        if done:
+            break
+    valid_inputs = next_valid_inputs
