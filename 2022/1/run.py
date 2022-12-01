9831dad32f28d9aeabb864b3a49b4e15fc5f01f5
Peter Cheng <petercheng00@gmail.com>
2022-11-30 22:47:32 -0800

    2022

---
+++ 2022/1/run.py
@@ -0,0 +1,40 @@
+with open("input.txt") as f:
+    lines = f.read().splitlines()
+
+def part1():
+    sum = 0
+    max_sum = 0
+    for line in lines:
+        if line.isnumeric():
+            sum += int(line)
+        else:
+            max_sum = max(max_sum, sum)
+            sum = 0
+    
+    print(max_sum)
+
+
+def part2():
+    sum = 0
+    max1_sum = 0
+    max2_sum = 0
+    max3_sum = 0
+    for line in lines:
+        if line.isnumeric():
+            sum += int(line)
+        else:
+            if sum >= max1_sum:
+                max3_sum = max2_sum
+                max2_sum = max1_sum
+                max1_sum = sum
+            elif sum >= max2_sum:
+                max3_sum = max2_sum
+                max2_sum = sum
+            elif sum > max3_sum:
+                max3_sum = sum
+            sum = 0
+    
+    print(max1_sum + max2_sum + max3_sum)
+
+part1()
+part2()
