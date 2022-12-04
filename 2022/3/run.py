29b7e85860acde6bf9a81b7e62476c950b43e564
Peter Cheng <petercheng00@gmail.com>
2022-12-03 18:28:10 -0800

    day3

---
+++ 2022/3/run.py
@@ -0,0 +1,30 @@
+with open("input", "r") as f:
+    lines = f.read().splitlines()
+
+
+def part1():
+    sum_priority = 0
+    for line in lines:
+        common = set(line[: len(line) // 2]) & set(line[len(line) // 2 :])
+        char = next(iter(common))
+        if char.islower():
+            sum_priority += ord(char) - ord("a") + 1
+        else:
+            sum_priority += ord(char) - ord("A") + 27
+    print(sum_priority)
+
+
+def part2():
+    sum_priority = 0
+    for l1, l2, l3 in zip(lines[::3], lines[1::3], lines[2::3]):
+        common = set(l1) & set(l2) & set(l3)
+        char = next(iter(common))
+        if char.islower():
+            sum_priority += ord(char) - ord("a") + 1
+        else:
+            sum_priority += ord(char) - ord("A") + 27
+    print(sum_priority)
+
+
+part1()
+part2()
